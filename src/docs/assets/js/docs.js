82e2f3cc95da41b7a7b8834c863a1012612d813c
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2021-09-01 12:48:15 +0800

    dynamic

--- src/docs/assets/js/docs.js
+++ src/docs/assets/js/docs.js
@@ -1,6 +1,9 @@
-(function(){
-    window.addEventListener('load', ()=>{
-       console.log('Load');
-
+(function () {
+    document.querySelector('#add-dynamic-content').addEventListener('click', () => {
+        let t = document.querySelector('#dynamic-content-template');
+        let time = t.content.querySelector('time');
+        time.textContent = new Date().toString();
+        let clone = document.importNode(t.content, true);
+        document.querySelector('#dynamic-content-wrapper').appendChild(clone);
     });
 })();
